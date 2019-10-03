3bc4258b4fe3f85f15f00dae60d4ffb5a4ced33b
real-dragos <dumitrescu.dragos.lucian87@gmail.com>
2019-10-03 11:48:40 +0300

    Updates

---
+++ precache-manifest.93a2e1cfcdafd3b93ec09a6811b8a1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e097f25ab864878ca3a4686a1eb2d492",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "18d6a642eb740b8a9879",
+    "url": "/Robo-Friends/static/css/main.715c69b9.chunk.css"
+  },
+  {
+    "revision": "209454b5dc29f645629a",
+    "url": "/Robo-Friends/static/js/2.67161f44.chunk.js"
+  },
+  {
+    "revision": "18d6a642eb740b8a9879",
+    "url": "/Robo-Friends/static/js/main.e412e11d.chunk.js"
+  },
+  {
+    "revision": "2740f215d9d48d7dcbcf",
+    "url": "/Robo-Friends/static/js/runtime~main.321acf5e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
